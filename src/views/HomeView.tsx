c20b245310d3c9f8ac5597edd98a4f28f414975f
Edson Alcala <edson_alcala@msn.com>
2021-04-18 23:05:00 +0100

    update video url

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -52,7 +52,7 @@ export const HomeView: React.FC = () => {
           </StyledLink>
         </Grid>
         <Grid item={true} xs={6} lg={6}>
-          <YouTube opts={opts} videoId="rmDlFUuprHg" onReady={onReady} />
+          <YouTube opts={opts} videoId="Ogrs1eQZSeA" onReady={onReady} />
         </Grid>
       </Grid>
     </WrapperContainer>
